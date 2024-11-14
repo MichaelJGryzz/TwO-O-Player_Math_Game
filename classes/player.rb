e7029c9d794fda1cf5353b8a76429a1d21aa82c5
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-11-13 23:41:29 -0500

    Fix typos in comments

--- classes/player.rb
+++ classes/player.rb
@@ -1,13 +1,13 @@
 class Player
   attr_reader :name, :lives
 
-  # Method sets up the player's name and their initial numvber of lives (3).
+  # Method sets up the player's name and their initial number of lives (3).
   def initialize(name)
     @name = name
     @lives = 3
   end
 
-  # Method decreases the player's lives by 1 whenevr they lose a life (answer a question wrong).
+  # Method decreases the player's lives by 1 whenever they lose a life (answer a question wrong).
   def lose_life
     @lives -= 1
   end
